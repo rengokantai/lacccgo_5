ac833cec7f38f7d6e061e605a40428e1ed7d95dd
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-02 17:22:00 -0400

    basic usage of exif

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ recursion, file manipulation
 ######1
 resource
 [the way to Go](https://sites.google.com/site/thewaytogo2012/Downhome/Topic3)
+
+######5
+use third party
+```
+go get -u github.com/rwcarlsen/goexif/exif
+```
--- src/main.go
+++ src/main.go
@@ -3,6 +3,10 @@ import (
 "fmt"
 "os"
 "path/filepath"
+	"log"
+	//"image/jpeg"
+	"github.com/rwcarlsen/goexif/exif"
+	"io/ioutil"
 )
 
 func main() {
@@ -17,8 +21,40 @@ ext := filepath.Ext(path)
 
 switch ext {
 case ".jpg", ".jpeg":
-fmt.Println(ext)
+	fmt.Println(ext)
+
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+//	img, err := jpeg.Decode(f)
+//	r, g, b, a := img.At(0, 0).RGBA()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Printf("%d %d %d %d \n", r, g, b, a)
+
+
+	xi(f)
 }
 return nil
 })
+}
+
+func xi(f *os.File) {
+	x, err := exif.Decode(f)
+	if err != nil {
+		log.SetOutput(ioutil.Discard)
+		log.Println(err)
+	}
+	if x != nil {
+		tm, _ := x.DateTime()
+		fmt.Println("Taken: ", tm)
+
+		lat, long, _ := x.LatLong()
+		fmt.Println("lat, long: ", lat, ", ", long)
+		fmt.Println(x.String())
+	}
 }
